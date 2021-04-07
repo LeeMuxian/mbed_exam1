dbd026d0c5d006b735857def13c63b9521843b8d
Lee_Muxian <lee0906926618@gmail.com>
2021-04-07 01:03:44 -0700

    add main.cpp

---
+++ main.cpp
@@ -0,0 +1,103 @@
+#include "mbed.h"
+#include "uLCD_4DGL.h"
+
+DigitalIn Up(D10);
+DigitalIn Down(D11);
+DigitalIn Select(D12);
+uLCD_4DGL uLCD(D1, D0, D2);
+AnalogOut wave_out(D7);
+AnalogIn sample_wave(A5);
+Thread thread;
+float ADCdata[500];
+// Timer time;
+
+void sample(void)
+{
+    // time.start();
+    for(int i = 0; i < 500; i++) {          // Use about 3 seconds for sampling 500 times.
+        ADCdata[i] = sample_wave;           // Thus, the average sampling rate is 166.6666667Hz.
+        ThisThread::sleep_for(2ms);
+    }
+    // time.stop();
+    // auto s = chrono::duration_cast<chrono::seconds>(t.elapsed_time()).count();
+    // printf("Timer time: %llus\n", s);
+    for (int i = 0; i < 500; i++) {
+        printf("%f\r\n", ADCdata[i]);
+    }
+}
+
+int main()
+{
+    float rate = 1/8;
+    int i, j;
+    int T = 240, up_time, down_time, in_time;          // T means the period of wave.
+                                                       // up_time means the time of the upward region of one period. 
+    double amp;
+
+    wave_out = 0;
+
+    i = ((SIZE_X / 5) - 6) / 2 - 1;
+    j = ((SIZE_Y / 7) - 1) / 2 - 1;
+
+    uLCD.background_color(WHITE);
+    uLCD.cls();
+    uLCD.textbackground_color(WHITE);
+    uLCD.color(BLACK);
+    uLCD.set_font(FONT_5X7);
+    uLCD.locate(i, j);
+    uLCD.printf("%lf", rate * 8);
+
+    while(1) {
+        if (Up == 1) {
+            if (rate < 1)
+                rate *= 2;
+            else
+                rate = 1;
+            uLCD.cls();
+            uLCD.locate(i, j);
+            uLCD.printf("%lf", rate);
+            ThisThread::sleep_for(500ms);
+        }
+        else if (Down == 1) {
+            if (rate > 1 / 8)
+                rate /= 2;
+            else
+                rate = 1 / 8;
+            uLCD.cls();
+            uLCD.locate(i, j);
+            uLCD.printf("%lf", rate);
+            ThisThread::sleep_for(500ms);
+        }
+        if (Select == 1)
+            break;
+    }
+
+    up_time = 80 * rate;
+    down_time = 80 * rate;
+    in_time = T - up_time - down_time;
+    amp = 3 / 3.3;
+
+    while(1) {
+        for (float a = 0.0; a < amp; a += amp / (up_time / 0.0000241))
+            wave_out = a;
+        ThisThread::sleep_for(in_time);
+        for (float b = amp; b > 0; b -= amp / (down_time / 0.0000241))
+            wave_out = b;
+    }
+
+    /*T = 1 / (double)freq;
+    up_time = T / 5;
+    down_time = (T / 5) * 4;
+    amp = 3 / 3.3;
+
+    thread.start(sample);
+
+    while(1) {
+        for(float a = 0.0; a < amp; a += amp / (up_time / 0.0000241)) {     // we try and error and find out that 0.0000237 is the best value.
+            wave_out = a;
+        }
+        for(float b = amp; b > 0; b -= amp / (down_time / 0.0000241)) {
+            wave_out = b;
+        }
+    }*/
+}
